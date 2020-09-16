991b8cca94ae85ba25ebf6183cc579e00ff8e258
wolfie Dedev <wolfiededev@wolfies-iMac.local>
2020-09-16 16:47:39 +0100

    implemented JavaScript for login-register section, & also got rid of a container div & another div around the login-register-content that were not allowing the background to close the entire login page when cliked

--- dist/js/order.js
+++ dist/js/order.js
@@ -1,7 +1,26 @@
+// LOGIN REGISTER PAGE
 
+const loginRegister = document.querySelector('#login-register');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+// const loginBox = document.querySelector('.login-register-box');
+const close = document.querySelector('.close');
 
-    // Declare Variables 
-    const registerPage = document.querySelector('#register');
-    const signupLink = document.querySelector('.sign-up-link')
+console.log(loginRegister, loginLink, registerLink, close);
 
-    console.log(registerPage, signupLink)
+// Show login area when login link is clicked
+loginLink.addEventListener('click', () => {
+    loginRegister.style.display = 'block';
+} );
+
+// Close login area with the x icon 
+close.addEventListener('click', () => {
+    loginRegister.style.display = 'none';
+});
+
+// Close login area when the login background is cliked
+loginRegister.addEventListener('click', (e) => {
+    if(e.target.className == 'login-bg'){
+        loginRegister.style.display = 'none';
+    }
+});
