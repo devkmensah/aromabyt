61ed727a90a72bbecb3350101c2b5250d05f328a
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2020-09-16 14:37:27 +0100

    created JavaScript file for order.html page

---
+++ dist/js/order.js
@@ -0,0 +1,7 @@
+
+
+    // Declare Variables 
+    const registerPage = document.querySelector('#register');
+    const signupLink = document.querySelector('.sign-up-link')
+
+    console.log(registerPage, signupLink)
