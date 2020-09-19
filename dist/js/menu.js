caac949bc376190028ab5e5fc166fdf27225d731
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2020-09-19 17:13:15 +0100

    styled the menu-list area in the menu page to be fixed onscroll when it reaches the top of the page & implemented JavaScript for it

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -1,4 +1,4 @@
-// LOGIN PAGE 
+//1. LOGIN PAGE 
 const loginRegister = document.querySelector('#login-register');
 const loginLink = document.querySelector('.login-link');
 const close = document.querySelector('.close');
@@ -6,7 +6,7 @@ const close = document.querySelector('.close');
 // Show login area when login link is clicked
 loginLink.addEventListener('click', () => {
     loginRegister.style.display = 'block';
-} );
+});
 
 // Close login area with the x icon on click
 close.addEventListener('click', () => {
@@ -15,14 +15,14 @@ close.addEventListener('click', () => {
 
 // Close login area when the login background is cliked
 loginRegister.addEventListener('click', (e) => {
-    if(e.target.id == 'login-register'){
+    if (e.target.id == 'login-register') {
         loginRegister.style.display = 'none';
     }
 });
 
 
 
-// REGISTER PAGE 
+//2. REGISTER PAGE 
 const register = document.querySelector('#register');
 const registerLink = document.querySelector('.register-link');
 const closeRegister = document.querySelector('.close-register');
@@ -45,8 +45,30 @@ closeRegister.addEventListener('click', () => {
 
 // Close register when background is clicked
 register.addEventListener('click', (e) => {
-    if(e.target.id === 'register'){
+    if (e.target.id === 'register') {
         register.style.display = 'none';
     }
 })
 
+
+//3. STICK MENU-LIST AREA TO THE TOP OF SCREEN ON SCROLL
+// & PREVENT SUDDEN QUICK MOVEMENT OF LUNCH MENU HEADER TITLE UNDER MENU-LIST WHEN FIXED EFFECT IS ADDED TO THE MENU-LIST ID  
+const menuList = document.querySelector("#menu-list");
+const pageTop = menuList.offsetTop;
+const smoothPosition = document.querySelector('#menu');
+console.log(smoothPosition, pageTop);
+
+const stickMenu = () => {
+    if (pageYOffset >= pageTop) {
+        menuList.classList.add("sticky");
+        smoothPosition.classList.add('smooth-position');
+    } else {
+        menuList.classList.remove("sticky");
+        smoothPosition.classList.remove('smooth-position');
+    }
+}
+
+
+onscroll = () => {
+    stickMenu()
+}
