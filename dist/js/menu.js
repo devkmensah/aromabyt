16db57db3d873fa068fb6132b0276ceb90bbaaf8
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2020-09-22 17:42:25 +0100

    redid the header or navigation area for order page

--- dist/js/menu.js
+++ dist/js/menu.js
@@ -75,15 +75,15 @@ onscroll = () => {
 
 
 // HAMBURGER MENU
-const openHamburger = document.querySelector('#hamburger-toggler');
-const mobileNav = document.querySelector('.sidenav');
+// const openHamburger = document.querySelector('#hamburger-toggler');
+// const mobileNav = document.querySelector('.sidenav');
 
-function openMenu(e){
-    this.querySelector('.burger-icon').classList.toggle('fa-bars');
-    this.querySelector('.burger-icon').classList.toggle('fa-times');
-    mobileNav.classList.toggle('show');
-}
+// function openMenu(e){
+//     this.querySelector('.burger-icon').classList.toggle('fa-bars');
+//     this.querySelector('.burger-icon').classList.toggle('fa-times');
+//     mobileNav.classList.toggle('show');
+// }
 
-openHamburger.addEventListener('click', openMenu);
+// openHamburger.addEventListener('click', openMenu);
 
-console.log(openHamburger, mobileNav)
+// console.log(openHamburger, mobileNav)
