bb11ad6bdb4b7693c613500304d4c52c681bbd7b
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2021-03-08 00:17:07 +0000

    coded & refactored the delivery & i'll collect order functionality located in the order.html page

--- dist/js/order.js
+++ dist/js/order.js
@@ -1,95 +1,81 @@
-// LOGIN PAGE
-const loginRegister = document.querySelector('#login-register');
-const loginLink = document.querySelector('.login-link');
-const close = document.querySelector('.close');
+// // LOGIN PAGE
+// const loginRegister = document.querySelector('#login-register');
+// const loginLink = document.querySelector('.login-link');
+// const close = document.querySelector('.close');
 
-// Show login area when login link is clicked
-loginLink.addEventListener('click', () => {
-    loginRegister.style.display = 'block';
-});
+// // Show login area when login link is clicked
+// loginLink.addEventListener('click', e => {
+//     loginRegister.style.display = 'block';
+// });
 
-// Close login area with the x icon on click
-close.addEventListener('click', () => {
-    loginRegister.style.display = 'none';
-});
+// // Close login area with the x icon on click
+// close.addEventListener('click', () => {
+//     loginRegister.style.display = 'none';
+// });
 
-// Close login area when the login background is cliked
-loginRegister.addEventListener('click', e => {
-    if (e.target.id == 'login-register') {
-        loginRegister.style.display = 'none';
-    }
-});
+// // Close login area when the login background is cliked
+// loginRegister.addEventListener('click', e => {
+//     if (e.target.id == 'login-register') {
+//         loginRegister.style.display = 'none';
+//     }
+// });
 
-// REGISTER PAGE
-const register = document.querySelector('#register');
-const closeRegister = document.querySelector('.close-register');
-const registerLinkOther = document.querySelector('.register-link-other');
-// const createAccount = document.querySelector('.create');
-const yetLink = document.querySelector('.yet-link');
+// // REGISTER PAGE
+// const register = document.querySelector('#register');
+// const closeRegister = document.querySelector('.close-register');
+// const registerLinkOther = document.querySelector('.register-link-other');
+// // const createAccount = document.querySelector('.create');
+// const yetLink = document.querySelector('.yet-link');
 
-// Show register page when register link is clicked
-// registerLink.addEventListener('click', () => {
+// // Show register page when register link is clicked
+// // registerLink.addEventListener('click', () => {
+// //     register.style.display = 'block';
+// // });
+
+// // Show register page when create class is clicked
+// // createAccount.addEventListener('click', () => {
+// //     register.style.display = 'block';
+// // });
+
+// // Show register page when yet link is clicked
+// yetLink.addEventListener('click', () => {
 //     register.style.display = 'block';
 // });
 
-// Show register page when create class is clicked
-// createAccount.addEventListener('click', () => {
+// // Show register page when register link is clicked on the login page
+// registerLinkOther.addEventListener('click', () => {
 //     register.style.display = 'block';
 // });
 
-// Show register page when yet link is clicked
-yetLink.addEventListener('click', () => {
-    register.style.display = 'block';
-});
+// // Close register page when x icon is clicked
+// closeRegister.addEventListener('click', () => {
+//     register.style.display = 'none';
+// });
 
-// Show register page when register link is clicked on the login page
-registerLinkOther.addEventListener('click', () => {
-    register.style.display = 'block';
-});
+// // Close register when background is clicked
+// register.addEventListener('click', e => {
+//     if (e.target.id === 'register') {
+//         register.style.display = 'none';
+//     }
+// });
 
-// Close register page when x icon is clicked
-closeRegister.addEventListener('click', () => {
-    register.style.display = 'none';
-});
-
-// Close register when background is clicked
-register.addEventListener('click', e => {
-    if (e.target.id === 'register') {
-        register.style.display = 'none';
-    }
-});
-
-// DELIVERY PAGE
-const deliveryNav = document.querySelector('.delivery-nav');
-const collectNav = document.querySelector('.collect-nav');
-const delivery = document.querySelector('#delivery');
-const collect = document.querySelector('#collect');
-
-console.log(delivery, collect, deliveryNav, collectNav);
-
-const btn1 = document.querySelector('.btn-delivery')
-btn1.addEventListener('click', e => {
+// DELIVERY OR I'LL COLLECT ORDER
+const orderOptions = document.querySelector('.order-options');
+const orderLinks = document.querySelectorAll('.order-link');
+const orderContent = document.querySelectorAll('.order-link-content');
+console.log(orderOptions, orderLinks, orderContent);
+orderOptions.addEventListener('click', function (e) {
     e.preventDefault();
-    if ((e.target.className = 'delivery-nav')) {
-        delivery.style.display = 'block';
-    }
-    if ((e.target.className = 'collect-nav')) {
-        collect.style.display = 'none';
-    }
-    // if(e.target.className = 'delivery-nav'){
-    //     deliveryNav.style.color = '#73020C';
-    // }
-});
+    const clicked = e.target.closest('.order-link');
+    console.log(clicked);
 
-console.log(btn1, btn2);
-const btn2 = document.querySelector('.btn-collect')
-// COLLECT PAGE
-btn2.addEventListener('click', e => {
-    e.preventDefault()
-    if ((e.target.className = 'collect-nav')) {
-        collect.style.display = 'block';
-    }
-    if ((e.target.className = 'delivery-nav')) {
-        delivery.style.display = 'none';
-    }
+    if (!clicked) return;
+    orderLinks.forEach(link => link.classList.remove('order-link-active'));
+    orderContent.forEach(content =>
+        content.classList.remove('order-link-content-active')
+    );
+    clicked.classList.add('order-link-active');
+    const activeContent = document
+        .querySelector(`.order-link-content-${clicked.dataset.tab}`)
+        .classList.add('order-link-content-active');
 });
