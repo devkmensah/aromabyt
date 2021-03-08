7efde344c2c8a1f716629231101c7455b6f8205a
wolfie Dedev <wolfiededev@kodeds-iMac.localdomain>
2021-03-08 00:27:57 +0000

    refactoring the login form located in nthe order.html page

--- dist/js/order.js
+++ dist/js/order.js
@@ -1,26 +1,23 @@
-// // LOGIN PAGE
-// const loginRegister = document.querySelector('#login-register');
-// const loginLink = document.querySelector('.login-link');
-// const close = document.querySelector('.close');
+// LOGIN FORM 
+const loginRegister = document.querySelector('#login-register');
+const loginLink = document.querySelector('.login-link');
+const close = document.querySelector('.close');
 
-// // Show login area when login link is clicked
-// loginLink.addEventListener('click', e => {
-//     loginRegister.style.display = 'block';
-// });
+loginLink.addEventListener('click', e => {
+    loginRegister.style.display = 'block';
+});
 
-// // Close login area with the x icon on click
-// close.addEventListener('click', () => {
-//     loginRegister.style.display = 'none';
-// });
+close.addEventListener('click', () => {
+    loginRegister.style.display = 'none';
+});
 
-// // Close login area when the login background is cliked
-// loginRegister.addEventListener('click', e => {
-//     if (e.target.id == 'login-register') {
-//         loginRegister.style.display = 'none';
-//     }
-// });
+loginRegister.addEventListener('click', e => {
+    if (e.target.id === 'login-register') {
+        loginRegister.style.display = 'none';
+    }
+});
 
-// // REGISTER PAGE
+// // REGISTER FORM
 // const register = document.querySelector('#register');
 // const closeRegister = document.querySelector('.close-register');
 // const registerLinkOther = document.querySelector('.register-link-other');
