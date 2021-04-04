0570115db5839b4e6682deb21207ba13b94c4684
devkmensah <kmensah.dev@gmail.com>
2021-04-04 02:26:32 +0100

    applied safari webkit for delivery partners in homepage

--- dist/js/hamburger.js
+++ dist/js/hamburger.js
@@ -5,10 +5,10 @@
 const openHamburger = document.querySelector('#hamburger-toggler');
 const mobileNav = document.querySelector('.sidenav');
 
-function openMenu(e) {
+const openMenu = function (e) {
   this.querySelector('.burger-icon').classList.toggle('fa-bars');
   this.querySelector('.burger-icon').classList.toggle('fa-times');
   mobileNav.classList.toggle('show');
-}
+};
 
 openHamburger.addEventListener('click', openMenu);
